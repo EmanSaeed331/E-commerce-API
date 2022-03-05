3af515f067c0e132f9e0248fec65e01163f376dc
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-03-06 00:43:46 +0200

    add:middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../modules/Usermodule/models/userModel')
+const auth = async(req,res,next) =>{
+    try{
+    const token = req.header('Authorization').replace('Bearer','')
+    const decode = jwt.verify(token,process.env.JWT_SECRET_KEY)
+    const user = await User.findOne({id:decode._id,'tokens.token':token})
+
+    if(!user){
+        throw new Error()
+    }
+    req.token = token
+    req.user = user
+    next()
+    }
+    catch(e){
+        console.log(e)
+        res.status(404).send({error:e})
+    }
+}
+module.exports=auth
