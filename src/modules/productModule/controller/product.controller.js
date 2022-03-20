5a470c8026e8711af6db02d67e765a50a182df2f
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-03-20 18:29:12 +0200

    add:product controller

---
+++ src/modules/productModule/controller/product.controller.js
@@ -0,0 +1,9 @@
+const productModel = require('../model/productModel')
+
+// Create Product 
+// get Product 
+// Update product 
+// Delete product 
+// List product  (get all products )
+// add filter (simple filter sort by {name , price })
+// assign product to catalog 
