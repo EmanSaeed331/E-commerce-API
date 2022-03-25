4f8ac271540f4f80cecffa28f44637a3d486e82e
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-03-25 02:24:51 +0200

    add:category model

---
+++ src/modules/CategoryModule/model/category.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose') 
+var categorySchema = new mongoose.Schema({
+    "categoryName":{
+        type:String , 
+        required:true
+    },
+    "categoryDescription":{
+        type:String , 
+        required:true 
+    }
+})
+var categoryModel = new mongoose.Model('category',categorySchema)
+module.exports = categoryModel
