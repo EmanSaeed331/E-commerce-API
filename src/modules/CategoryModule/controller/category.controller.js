0a2211a81740f1f04064811a5f1de68cbb7315a3
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-03-25 02:33:04 +0200

    add:create category

---
+++ src/modules/CategoryModule/controller/category.controller.js
@@ -0,0 +1,19 @@
+const category = require('../model/category.model')
+
+// create Category 
+var createCategory = async(req,res)=>{
+    try{
+    var category = new category(req.body)
+    await category.save()
+
+    res.status(200).send(category)
+    }catch(e){
+        res.status(404).send({error:e})
+    }
+}
+// retrieve Category
+// delete Category
+// list Category
+// add products to Category
+
+module.exports = createCategory
