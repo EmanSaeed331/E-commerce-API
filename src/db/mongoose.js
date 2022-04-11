26f80bf4facd7e9cb37205a592489240a9e71d8e
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-04-11 20:54:04 +0200

    fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://'+ process.env.MONGODB_USER +':'+ process.env.MONGODB_PW +'@cluster0.ulobg.mongodb.net/E-commerce-app?retryWrites=true&w=majority';
 const LocalDB = 'mongodb://127.0.0.1:27017/E-commerce-app'
-mongoose.connect(connectionString , {
+mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology:true ,
     useNewUrlParser: true,
     useCreateIndex: true,
