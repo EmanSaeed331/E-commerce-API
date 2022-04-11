012f44e5ca2c0412b9770cc47ea52d141d6fcadb
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-04-11 07:10:20 +0200

    test

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const userRouter = require('./src/modules/Usermodule/router/user_router')
+const productRouter = require('./src/modules/productModule/router/product.router')
+const categoryRouter = require('.src/modules/CategoryModule/router/category.router')
+const reviewRouter = require('.src/modules/ReviewModule/router/review.router')
+const adminRouter = require('.src/modules/adminModule/router/admin.router')
+const port = process.env.PORT | 5000;
+require('./db/mongoose')
+app.use(express.json())
+app.use(userRouter)
+app.use(productRouter)
+app.use(categoryRouter)
+app.use(reviewRouter)
+app.use(adminRouter)
+app.get('/',(req,res)=>{
+    res.status({message:'Start using app'})
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port + ${port}`)
+})
