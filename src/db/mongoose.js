f26653012b343dc8dce5e10833be914efcb8d092
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-04-11 06:32:23 +0200

    update DB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/E-commerce-app' , {
+const connectionString = 'mongodb+srv://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PW + '@cluster0.ulobg.mongodb.net/E-commerce-app?retryWrites=true&w=majority';
+const LocalDB = 'mongodb://127.0.0.1:27017/E-commerce-app'
+mongoose.connect(connectionString , {
     useUnifiedTopology:true ,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
 
 })
+
+
